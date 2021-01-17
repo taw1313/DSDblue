e902ce10de438bc146d1e90d638a24acc1df1483
Todd Wilson <taw1313@gmail.com>
2021-01-17 15:03:40 -0500

    refactor device files and remove useasyncstorage for now

--- src/device/Bluetooth.js
+++ src/device/Bluetooth.js
@@ -1,127 +1,161 @@
-const SUUID = "0000ffe0-0000-1000-8000-00805f9b34fb"
-const CUUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+import {encode as btoa} from 'base-64';
 
-const DEBUGMODE = false 
+const SUUID = '0000ffe0-0000-1000-8000-00805f9b34fb';
+const CUUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
+
+const DEBUGMODE = false;
 
 const hexToBase64 = (h) => {
-  return btoa(h.replace(/0x/gi,'').match(/\w{2}/g).map(function(a) {
-      return String.fromCharCode(parseInt(a, 16));
-  }).join(""));
-}
+  return btoa(
+    h
+      .replace(/0x/gi, '')
+      .match(/\w{2}/g)
+      .map(function (a) {
+        return String.fromCharCode(parseInt(a, 16));
+      })
+      .join(''),
+  );
+};
 
-let self = module.exports = {
-
+let self = (module.exports = {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  checkStateAndConnect: ( manager ) => {
-    return new Promise ( (resolve, reject) => {
+  checkStateAndConnect: (manager) => {
+    return new Promise((resolve, reject) => {
       if (DEBUGMODE) {
         let device = {
-          id: "0"
-        }
-        resolve( device.id )
+          id: '0',
+        };
+        resolve(device.id);
       }
-      const subscription = manager.onStateChange( (stateOfDev) => {
-        console.log('DEBUG - Bluetooth.js checkStateAndConnect() stateOfDev ', stateOfDev)
-        if( stateOfDev === 'PoweredOn') {
+      const subscription = manager.onStateChange((stateOfDev) => {
+        console.log(
+          'DEBUG - Bluetooth.js checkStateAndConnect() stateOfDev ',
+          stateOfDev,
+        );
+        if (stateOfDev === 'PoweredOn') {
           /*  Moving this... need add logic if the phone's bluetooth stat is not "PoweredOn"
           self.scanAndConnect( manager )
             .then( (device) => resolve( device )  )
             .catch( (err) => reject('ERROR - could not connect ', err) )
           subscription.remove()
           */
-          resolve( 'done' )
+          resolve('done');
         }
-      }, true)
-    })
+      }, true);
+    });
   },
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  scanAndConnect: ( manager, dev )  => {
-    return new Promise ( (resolve, reject) => {
+  scanAndConnect: (manager, dev) => {
+    return new Promise((resolve, reject) => {
       manager.startDeviceScan(null, null, (error, bleDev) => {
         if (error) {
           // Handle error (scanning will be stopped automatically)
-          console.log('ERROR - startDeviceScan()', error)
-          return
+          console.log('ERROR - startDeviceScan()', error);
+          return;
         }
 
-        console.log('DEBUG - Bluetooth.js scanAndConnect() ', bleDev.id, dev.id)
+        console.log(
+          'DEBUG - Bluetooth.js scanAndConnect() ',
+          bleDev.id,
+          dev.id,
+        );
         // Check if it is a device you are looking for based on advertisement data
         // or other criteria.
-        if (bleDev.id === dev.id) {    
-
+        if (bleDev.id === dev.id) {
           // Stop scanning as it's not necessary if you are scanning for one device.
-          manager.stopDeviceScan()
+          manager.stopDeviceScan();
 
           // Proceed with connection.
-          self.connectToBleDevice( bleDev )
-            .then (device => resolve(device) )
-            .catch( (err) => reject('ERROR - could not connect ', err) )
+          self
+            .connectToBleDevice(bleDev)
+            .then((device) => resolve(device))
+            .catch((err) => reject('ERROR - could not connect ', err));
         }
-      })
-
-    })
+      });
+    });
   },
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   connectToBleDevice: (bleDev) => {
-    return new Promise ( (resolve, reject) => {
-          bleDev.connect()
-            .then( (d) => {
-              return d.discoverAllServicesAndCharacteristics()
-            })
-            .then( (device) => {
-              if (device.name === null || device.name.length < 2) device.name = device.id
-              if (device.localName === null  || device.name.length < 2) device.localName = device.name
-              resolve( device )
-            })
-            .catch( (err) => reject('ERROR - could not connect ', err) )
-    })
-
+    return new Promise((resolve, reject) => {
+      bleDev
+        .connect()
+        .then((d) => {
+          return d.discoverAllServicesAndCharacteristics();
+        })
+        .then((device) => {
+          if (device.name === null || device.name.length < 2) {
+            device.name = device.id;
+          }
+          if (device.localName === null || device.name.length < 2) {
+            device.localName = device.name;
+          }
+          resolve(device);
+        })
+        .catch((err) => reject('ERROR - could not connect ', err));
+    });
   },
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  scanForDevices: ( manager, addDevice ) => {
-      manager.startDeviceScan(null, null, (error, bleDev) => {
-        if (error) {
-          // Handle error (scanning will be stopped automatically)
-          console.log('ERROR - startDeviceScan()', error)
-          return
-        }
+  scanForDevices: (manager, addDevice) => {
+    manager.startDeviceScan(null, null, (error, bleDev) => {
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        console.log('ERROR - startDeviceScan()', error);
+        return;
+      }
 
-        if (bleDev.name === null || bleDev.name.length < 2) bleDev.name = bleDev.id
-        if (bleDev.localName === null  || bleDev.name.length < 2) bleDev.localName = bleDev.name
-        addDevice( bleDev )
-      })
-
+      if (bleDev.name === null || bleDev.name.length < 2) {
+        bleDev.name = bleDev.id;
+      }
+      if (bleDev.localName === null || bleDev.name.length < 2) {
+        bleDev.localName = bleDev.name;
+      }
+      addDevice(bleDev);
+    });
   },
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   sendOnToBluetooth: (manager, devId, channel) => {
-    let bV = [ 
-      "oAEBog==",  // 0xA00101A2 | oAEBog==
-      "oAIBow=="   // 0xA00201A3 | oAEBog==
-    ]
+    let bV = [
+      'oAEBog==', // 0xA00101A2 | oAEBog==
+      'oAIBow==', // 0xA00201A3 | oAEBog==
+    ];
 
-    console.log('DEBUG - test ', devId, bV[channel], hexToBase64('0xA00101A2'), hexToBase64('0xA00101A3'))
-    if (DEBUGMODE) return('Debug')
-    manager.writeCharacteristicWithoutResponseForDevice( devId, SUUID, CUUID, bV[channel] )
-      .then( (res) => {
-        console.log('DEBUG - response after ON', res)
+    console.log(
+      'DEBUG - test ',
+      devId,
+      bV[channel],
+      hexToBase64('0xA00101A2'),
+      hexToBase64('0xA00101A3'),
+    );
+    if (DEBUGMODE) {
+      return 'Debug';
+    }
+    manager
+      .writeCharacteristicWithoutResponseForDevice(
+        devId,
+        SUUID,
+        CUUID,
+        bV[channel],
+      )
+      .then((res) => {
+        console.log('DEBUG - response after ON', res);
       })
-      .catch( (err) => {
-        console.log('ERROR - after ON ', err)
-      })
+      .catch((err) => {
+        console.log('ERROR - after ON ', err);
+      });
   },
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -129,50 +163,58 @@ let self = module.exports = {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   sendOffToBluetooth: (manager, devId, channel) => {
     let bV = [
-      "oAEAoQ==",   // 0xA00100A1 | oAEAoQ==
-      "oAIAog=="    // 0xA00200A2 | oAEAoQ==
-    ]
+      'oAEAoQ==', // 0xA00100A1 | oAEAoQ==
+      'oAIAog==', // 0xA00200A2 | oAEAoQ==
+    ];
 
-    if (DEBUGMODE) return('Debug')
-    manager.writeCharacteristicWithoutResponseForDevice( devId, SUUID, CUUID, bV[channel])
-      .then( (res) => {
-        console.log('DEBUG - response after OFF', res)
+    if (DEBUGMODE) {
+      return 'Debug';
+    }
+    manager
+      .writeCharacteristicWithoutResponseForDevice(
+        devId,
+        SUUID,
+        CUUID,
+        bV[channel],
+      )
+      .then((res) => {
+        console.log('DEBUG - response after OFF', res);
       })
-      .catch( (err) => {
-        console.log('ERROR - after OFF ', err)
-      })
+      .catch((err) => {
+        console.log('ERROR - after OFF ', err);
+      });
   },
 
   disconnectFromBleDevice: (manager, devId) => {
-    return new Promise ( (resolve, reject) => {
-        manager.cancelDeviceConnection(devId)
-        .then( (res) => {
-          resolve(res)
+    return new Promise((resolve, reject) => {
+      manager
+        .cancelDeviceConnection(devId)
+        .then((res) => {
+          resolve(res);
         })
-        .catch( (err) => {
-          reject(err)
-        })
-    })
-  }
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+});
 
-}
-
-    //
-    // DEBUG - walk the device's services and characteristics 
-    //
-    // DBG  manager.servicesForDevice( devId )
-    // DBG   .then( (res) => {
-    // DBG     console.log('DEBUG - services() ', res.length, res)
-    // DBG     for( let i=0; i<res.length; i++) {
-    // DBG       manager.characteristicsForDevice( devId, res[i].uuid )
-    // DBG         .then( (r) => {
-    // DBG           console.log(`DEBUG - characteristics(${res[i].uuid}) `, r)
-    // DBG         })
-    // DBG         .catch( (err) => {
-    // DBG           console.log(`ERROR - after characteristics(${res[i].uuid}) `, err)
-    // DBG         })
-    // DBG     } 
-    // DBG   })
-    // DBG   .catch( (err) => {
-    // DBG     console.log('ERROR - after services() ', err)
-    // DBG   })
+//
+// DEBUG - walk the device's services and characteristics
+//
+// DBG  manager.servicesForDevice( devId )
+// DBG   .then( (res) => {
+// DBG     console.log('DEBUG - services() ', res.length, res)
+// DBG     for( let i=0; i<res.length; i++) {
+// DBG       manager.characteristicsForDevice( devId, res[i].uuid )
+// DBG         .then( (r) => {
+// DBG           console.log(`DEBUG - characteristics(${res[i].uuid}) `, r)
+// DBG         })
+// DBG         .catch( (err) => {
+// DBG           console.log(`ERROR - after characteristics(${res[i].uuid}) `, err)
+// DBG         })
+// DBG     }
+// DBG   })
+// DBG   .catch( (err) => {
+// DBG     console.log('ERROR - after services() ', err)
+// DBG   })
--- src/device/asyncStorage.js
+++ src/device/asyncStorage.js
@@ -1,29 +1,36 @@
-import {useAsyncStorage} from '@react-native-community/async-storage';
+// import {useAsyncStorage} from '@react-native-community/async-storage';
 
-const {getItem, setItem, removeItem} = useAsyncStorage('@DSDblue_key');
+// const {getItem, setItem, removeItem} = useAsyncStorage('@DSDblue_key');
 
-let self = (module.exports = {
+module.exports = {
   readBleDevicesFromStorage: async () => {
     console.log(
       'DEBUG - asyncStorage.js readBleDevicesFromStorage before getItem() ',
     );
+    /*
     const items = await getItem();
     console.log('DEBUG - asyncStorage.js readBleDevicesFromStorage = ', items);
     return JSON.parse(items);
+    */
+    return [];
   },
 
   writeBleDevicesFromStorage: async (items) => {
+    /*
     await setItem(JSON.stringify(items));
+    */
     console.log('DEBUG - asyncStorage.js writeBleDevicesFromStorage = ', items);
     return items;
   },
 
   removeBleDevicesFromStorage: async () => {
+    /*
     try {
       await removeItem();
     } catch (e) {
       alert('ERROR - removing device from storage');
     }
+    */
     return 'Done';
   },
-});
+};
