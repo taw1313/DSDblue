9290e3558419781bcd304d6c64eed91df72dad4c
Todd Wilson <taw1313@gmail.com>
2021-01-17 12:35:32 -0500

    App style change

--- App.js
+++ App.js
@@ -1,42 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {Platform} from 'react-native';
 
-import React from 'react'
-import {Platform} from 'react-native'
-
-import MainPage from './src/pages/MainPage'
-import AuthPage from './src/pages/AuthPage'
+import MainPage from './src/pages/MainPage';
+import AuthPage from './src/pages/AuthPage';
 
 export default class App extends React.Component {
   state = {
     authToAccessBle: false,
-    osType: Platform.OS
-  }
+    osType: Platform.OS,
+  };
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   changeAuthAccessState = (newState) => {
-    this.setState({ authToAccessBle: newState })
-  }
+    this.setState({authToAccessBle: newState});
+  };
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   render() {
-    console.log('DEBUG - App.js render()', this.state.authToAccessBle)
-    if (this.state.authToAccessBle) 
-      return( 
-        <MainPage osType={this.state.osType}/> 
-      )
-    else 
-      return( 
-        <AuthPage osType={this.state.osType} changeState={this.changeAuthAccessState}/> 
-      )
+    console.log('DEBUG - App.js render()', this.state.authToAccessBle);
+    if (this.state.authToAccessBle) {
+      return <MainPage osType={this.state.osType} />;
+    } else {
+      return (
+        <AuthPage
+          osType={this.state.osType}
+          changeState={this.changeAuthAccessState}
+        />
+      );
     }
+  }
 }
