0217c04327278edac4436e895ff8819a9b855c0b
Todd Wilson <taw1313@gmail.com>
2021-02-05 12:05:40 -0500

    icon work

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,19 +1,13 @@
-import {Button, Icon, Left} from 'native-base';
+import {Button, Left} from 'native-base';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function HeaderButton({leftHeaderFunction}) {
   return (
     <Left>
       <Button style={styles.buttonStyle} onPress={leftHeaderFunction}>
-        <Icon name="ios-menu" />
-        {/*
-        <Icon
-          type="MaterialCommunityIcons"
-          style={styles.iconStyle}
-          name="menu"
-        />
-        */}
+        <Icon style={styles.iconStyle} name="menu" />
       </Button>
     </Left>
   );
@@ -21,10 +15,11 @@ function HeaderButton({leftHeaderFunction}) {
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    backgroundColor: '#614031',
+    // backgroundColor: '#614031',
   },
   iconStyle: {
     color: '#050505',
+    fontSize: 20,
   },
 });
 
